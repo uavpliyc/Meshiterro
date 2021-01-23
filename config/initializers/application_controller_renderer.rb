e239624c33d0c0f803463699cff8d55c1307a37f
uavpliyc <uavpliyc@gmail.com>
2021-01-23 09:37:23 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9fb46c0ba8836c7aa170aa3fc6543e758a99da5675fbc2ee592bebc9f4e286bb623795736b7d4aa8dfa70d19a5e060f62b47576e3b761f94512d24167a7a80a5'
